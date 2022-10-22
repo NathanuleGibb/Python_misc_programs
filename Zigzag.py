5b34bac5d9c7a6f58e0daa0007eb6a7913b3d7e2
NathanuleGibb <113156523+NathanuleGibb@users.noreply.github.com>
2022-10-22 15:08:09 +1100

    Add files via upload

---
+++ Zigzag.py
@@ -0,0 +1,25 @@
+# Write your code here :-)
+import time, sys
+indent = 0
+indentIncreasing = True
+
+try:
+    while True:
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(0.1)
+
+        if indentIncreasing:
+
+            indent = indent + 1
+            if indent == 20:
+                # change direction
+                indentIncreasing = False
+        else:
+            indent = indent - 1
+            if indent == 0:
+            # change direction
+                indentIncreasing = True
+except Keyboard.Interuption:
+    sys.exit()
+
